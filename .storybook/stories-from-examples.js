8c302f4ddd002dd4f046690bf7c9bc920b1675ae
Ben Scott <ben.scott@shopify.com>
2018-12-11 11:28:50 -0800

    Create 'All examples for a component' stories

--- .storybook/stories-from-examples.js
+++ .storybook/stories-from-examples.js
@@ -3,15 +3,59 @@ import {storiesOf, addDecorator} from '@storybook/react';
 import * as Polaris from '../src';
 
 // eslint-disable-next-line shopify/strict-component-boundaries
-import {components} from '../src/components/README.md';
+import {components as componentsFromReadMe} from '../src/components/README.md';
 
-addDecorator((story) => <Polaris.AppProvider>{story()}</Polaris.AppProvider>);
+const components = hydrateExecutableExamples(componentsFromReadMe);
+components.forEach(generateStoriesForComponent);
 
-components.forEach((component) => {
+function generateStoriesForComponent(component) {
+  storiesOf(component.name, module)
+    .addDecorator(AppProviderDecorator)
+    .add('All Examples', () => AllExamplesStoryForComponent(component));
+
   component.examples.forEach((example) => {
-    const StoryComponent = example.code({React, ...Polaris});
-    storiesOf(component.name, module).add(example.name, () => (
-      <StoryComponent />
-    ));
+    storiesOf(component.name, module)
+      .addDecorator(AppProviderDecorator)
+      .add(example.name, () => <example.Component />);
   });
-});
+}
+
+function hydrateExecutableExamples(components) {
+  return components.map((component) => {
+    component.examples = component.examples.map((example) => {
+      example.Component = example.code({React, ...Polaris});
+      return example;
+    });
+
+    return component;
+  });
+}
+
+function AppProviderDecorator(story) {
+  return <Polaris.AppProvider>{story()}</Polaris.AppProvider>;
+}
+
+/**
+ * A React component that renders all examples for a given component
+ * We only screenshot this with Percy instead of every example individually to
+ * keep the costs down as they charge per screenshot.
+ */
+function AllExamplesStoryForComponent(component) {
+  // Prevent false positives in visual regression testing.
+  // Set a minimum height so that examples don't shift and triger
+  // a failure if an example above them changes height
+  const containerStyle = {minHeight: '720px'};
+
+  return (
+    <React.Fragment>
+      {component.examples.map((example) => (
+        <React.Fragment key={example.name}>
+          <div style={containerStyle}>
+            <example.Component />
+          </div>
+          <hr />
+        </React.Fragment>
+      ))}
+    </React.Fragment>
+  );
+}
